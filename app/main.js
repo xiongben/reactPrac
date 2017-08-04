1c7666c50c7aafa6bb42822a50c900be84255e35
xiongben <1633387024@qq.com>
2017-08-04 18:29:16 +0800

    add

--- app/main.js
+++ app/main.js
@@ -11,14 +11,50 @@ import {
 } from 'react-router-dom';
 import './main.css';
 
+let testcss={
+  background:'red',
+  color:'yellow',
+  fontSize:'28px'
+};
+
+let testArr=[
+  <h1>this is first title!</h1>,
+  <h2>this is second title!</h2>,
+  <h3>this is third title!</h3>
+]
+
+let helloMessage=React.createClass({
+  render:function(){
+    return <h2>{this.props.name}</h2>;
+  }
+});
+
 const App = React.createClass({
   render() {
+    var text=this.state.liked?"喜欢":"不喜欢";
     return (
       <div>
-        <h1>App</h1>
-
+        <h1 style={testcss}>{this.props.title}</h1>
+        <div>{testArr}</div>
+        <a href={this.props.url}>{this.props.desc}</a>
+        <helloMessage name={this.props.title}/>
+        <p>--------------------------------我是分割线-----------------------</p>
+        <a onClick={this.testClick}>点击我会改变喜欢的状态：{text}</a>
       </div>
     )
+  },
+  testClick(){
+    this.setState({liked:!this.state.liked});
+  },
+  getInitialState(){
+    return {liked:false};
+  },
+  getDefaultProps(){
+    return {
+      title:"我是新的标题党啊",
+      desc:"我是新的描述啊",
+       url:"http://www.runoob.com"
+    }
   }
 });
 
@@ -42,34 +78,5 @@ const BasicExample = () => (
 )
 
 
-//
-// var Else=require.ensure([], () => {
-//       var Profile = require('./src/else/else');
-//       return Profile;
-//     });
-//     var Main=require.ensure([], () => {
-//           var Profile = require('./src/index/index2');
-//           return Profile;
-//         });
-//         var Login=require.ensure([], () => {
-//               var Profile = require('./src/login/login');
-//               return Profile;
-//             });
-render(<BasicExample />, document.getElementById('root'));
-// class App extends Component {
-//   constructor() {
-//     this.state = { currentComponent: Greeter };
-//   }
-//   openProfile() {
-//     require.ensure([], () => {
-//       var Profile = require('./src/index/index2');
-//       this.setState({
-//         currentComponent: Profile
-//       });
-//     });
-//   }
-//   render() {
-//       return <div>{this.state.currentComponent()}</div>
-//   }
-// };
-// render(<App/>,document.getElementById('root'));
+
+render(<BasicExample/>, document.getElementById('root'));
